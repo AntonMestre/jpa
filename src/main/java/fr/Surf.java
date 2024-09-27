7c3ec423c965a0e7ff50aa9c7348af8c4f7c8b5a
antoninmaystre <anmaystre@gmail.com>
2024-09-27 17:26:48 +0200

    chore: adding the surf entity

--- src/main/java/fr/Surf.java
+++ src/main/java/fr/Surf.java
@@ -1,5 +1,18 @@
 package fr;
 
+import java.util.UUID;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
 public class Surf {
     
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    private String name;
+    private Double initialPrice;
 }
